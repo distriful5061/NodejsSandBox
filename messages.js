d3164130433cae4166c945871ff9e26075f78de2
distriful5061 <watamituofiical@gmail.com>
2022-07-04 12:55:31 +0900

    a

---
+++ messages.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let messages = [
+    "1",
+    "2",
+    "3"
+]
---
+++ positiveMessages.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let messages = [
+    "1",
+        "2",
+            "3"
+            ]
+            
+messages.forEach(message => console.log(message));
